b94465ccfe0b67e67a616331af8d977ae5489f87
Edilson Alves <email@edilsonalves.com>
2020-04-16 03:34:42 -0300

    Format Home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,14 +1,49 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StatusBar, Text } from 'react-native';
+
+import Row from '../../components/Row';
+import Button from '../../components/Button';
 
 import styles from './styles';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontFamily: 'iria-sans-bold' }}>Home Screen</Text>
-      <Text style={{ fontFamily: 'roboto-mono-medium' }}>Home Screen</Text>
-      <Text style={{ fontFamily: 'roboto-mono-regular' }}>Home Screen</Text>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.display}>
+        <Text style={styles.value}>1989</Text>
+      </View>
+      <View style={styles.keyboard}>
+        <Row>
+          <Button onPress={() => {}} text={'C'}></Button>
+          <Button onPress={() => {}} text={'+/-'}></Button>
+          <Button onPress={() => {}} text={'%'}></Button>
+          <Button onPress={() => {}} text={'/'}></Button>
+        </Row>
+        <Row>
+          <Button onPress={() => {}} text={'7'}></Button>
+          <Button onPress={() => {}} text={'8'}></Button>
+          <Button onPress={() => {}} text={'9'}></Button>
+          <Button onPress={() => {}} text={'x'}></Button>
+        </Row>
+        <Row>
+          <Button onPress={() => {}} text={'4'}></Button>
+          <Button onPress={() => {}} text={'5'}></Button>
+          <Button onPress={() => {}} text={'6'}></Button>
+          <Button onPress={() => {}} text={'-'}></Button>
+        </Row>
+        <Row>
+          <Button onPress={() => {}} text={'1'}></Button>
+          <Button onPress={() => {}} text={'2'}></Button>
+          <Button onPress={() => {}} text={'3'}></Button>
+          <Button onPress={() => {}} text={'+'}></Button>
+        </Row>
+        <Row>
+          <Button onPress={() => {}} text={'0'}></Button>
+          <Button onPress={() => {}} text={'.'}></Button>
+          <Button onPress={() => {}} text={'='}></Button>
+        </Row>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -3,8 +3,34 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#ffffff',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  display: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#ffc300',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+
+  value: {
+    color: '#ffffff',
+    fontFamily: 'roboto-mono-medium',
+    fontSize: 72,
+    paddingTop: 100,
+    paddingHorizontal: 18,
+  },
+
+  keyboard: {
+    width: '100%',
+    height: 540,
+    backgroundColor: '#f7f8fa',
+    paddingTop: 18,
+    paddingBottom: 34,
+    paddingHorizontal: 18,
+    justifyContent: 'space-between',
   },
 });
